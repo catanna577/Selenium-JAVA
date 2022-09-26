ea93cdc485ee241f05fd7eea306a75f6cf1f003e
AnnaGushchina <anneta918@gmail.com>
2022-09-25 20:28:42 -0400

    added TC - win a game

--- src/test/java/samples/WordleTest.java
+++ src/test/java/samples/WordleTest.java
@@ -17,13 +17,13 @@ public class WordleTest {
 
     }
 
-    @Test         // TC - Verify that the user able loose a game
-    public void looseGame() throws InterruptedException {
+    @Test
+    // TC - Verify that the user able to lose a game
+    public void test1_loseGame() throws InterruptedException {
 
         // Navigate to the page and define the solution
         String solution = "apple";
-        navigateUrl("http://192.168.1.61:3000" + "/?test=" + solution);
-//        navigateUrl("http://localhost:3000" + "/?test=" + solution);
+        navigateUrl("http://localhost:3000" + "/?test=" + solution);
 
         // Input the 1st guess
         inputWord("fault");
@@ -38,18 +38,231 @@ public class WordleTest {
         // Input the 6th guess
         inputWord("slack");
 
-        // I wait for 3 sec
-        iWaitForSec(3);
+        // I wait for n sec
+        iWaitForSec(2);
 
         // Verify that the pop-up window will appear
-        //*[contains(text(),'Sorry, you loose!')]/.. - pop-up window
         elementWithXpathIsDisplayed("//*[contains(text(),'Sorry, you loose!')]/..");
 
         // Pop-up window contains the following text: "Sorry, you loose!"
-        //*[contains(text(),'Sorry, you loose!')] - message
         String text = "Sorry, you loose!";
         elementWithXpathContainsText("//*[contains(text(),'Sorry, you loose!')]", text);
-//        System.out.println(elementWithXpath("//*[contains(text(),'Sorry, you loose!')]").getText());
+
+    }
+
+    @Test
+    // TC - Win the game on the N try
+    public void test2_WinGameForOneGuess() throws InterruptedException {
+
+        // Navigate to the page and define the solution
+        String solution = "apple";
+        navigateUrl("http://localhost:3000" + "/?test=" + solution);
+
+        // Input the 1st guess
+        inputWord(solution);
+
+        // I wait for 2 sec
+        iWaitForSec(2);
+
+        // Verify that the pop-up window will appear
+        elementWithXpathIsDisplayed("//*[contains(text(),'You Win!')]/..");
+
+        // Pop-up window contains the following text: "You win!"
+        String header = "You Win!";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]", header);
+
+        // Pop-up window contains the following text: "The word was: solution"
+        elementWithXpathContainsText("//*[@class = 'solution']", solution);
+
+        // Pop-up window contains the following text: "You found the solution in N guesses :)"
+        String N = "1";
+        String guesses = "in " + N + " guesses";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]/../p[2]", guesses);
+
+    }
+
+    @Test
+    // TC - Win the game on the N try
+    public void test3_WinGameForTwoGuesses() throws InterruptedException {
+
+        // Navigate to the page and define the solution
+        String solution = "apple";
+        navigateUrl("http://localhost:3000" + "/?test=" + solution);
+
+        // Input the 1st guess
+        inputWord("fault");
+        // Input the 2nd guess
+        inputWord(solution);
+
+        // I wait for 2 sec
+        iWaitForSec(2);
+
+        // Verify that the pop-up window will appear
+        elementWithXpathIsDisplayed("//*[contains(text(),'You Win!')]/..");
+
+        // Pop-up window contains the following text: "You win!"
+        String header = "You Win!";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]", header);
+
+        // Pop-up window contains the following text: "The word was: solution"
+        elementWithXpathContainsText("//*[@class = 'solution']", solution);
+
+        // Pop-up window contains the following text: "You found the solution in N guesses :)"
+        String N = "2";
+        String guesses = "in " + N + " guesses";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]/../p[2]", guesses);
+
+    }
+
+    @Test
+    // TC - Win the game on the N try
+    public void test4_winGameForThreeGuesses() throws InterruptedException {
+
+        // Navigate to the page and define the solution
+        String solution = "apple";
+        navigateUrl("http://localhost:3000" + "/?test=" + solution);
+
+        // Input the 1st guess
+        inputWord("fault");
+        // Input the 2nd guess
+        inputWord("river");
+        // Input the 3rd guess
+        inputWord(solution);
+
+        // I wait for 2 sec
+        iWaitForSec(2);
+
+        // Verify that the pop-up window will appear
+        elementWithXpathIsDisplayed("//*[contains(text(),'You Win!')]/..");
+
+        // Pop-up window contains the following text: "You win!"
+        String header = "You Win!";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]", header);
+
+        // Pop-up window contains the following text: "The word was: solution"
+        elementWithXpathContainsText("//*[@class = 'solution']", solution);
+
+        // Pop-up window contains the following text: "You found the solution in N guesses :)"
+        String N = "3";
+        String guesses = "in " + N + " guesses";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]/../p[2]", guesses);
+
+    }
+
+    @Test
+    // TC - Win the game on the N try
+    public void test5_winGameForFourGuesses() throws InterruptedException {
+
+        // Navigate to the page and define the solution
+        String solution = "apple";
+        navigateUrl("http://localhost:3000" + "/?test=" + solution);
+
+        // Input the 1st guess
+        inputWord("fault");
+        // Input the 2nd guess
+        inputWord("river");
+        // Input the 3rd guess
+        inputWord("hobby");
+        // Input the 4th guess
+        inputWord(solution);
+
+        // I wait for 2 sec
+        iWaitForSec(2);
+
+        // Verify that the pop-up window will appear
+        elementWithXpathIsDisplayed("//*[contains(text(),'You Win!')]/..");
+
+        // Pop-up window contains the following text: "You win!"
+        String header = "You Win!";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]", header);
+
+        // Pop-up window contains the following text: "The word was: solution"
+        elementWithXpathContainsText("//*[@class = 'solution']", solution);
+
+        // Pop-up window contains the following text: "You found the solution in N guesses :)"
+        String N = "4";
+        String guesses = "in " + N + " guesses";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]/../p[2]", guesses);
+
+    }
+
+    @Test
+    // TC - Win the game on the N try
+    public void test6_winGameForFiveGuesses() throws InterruptedException {
+
+        // Navigate to the page and define the solution
+        String solution = "apple";
+        navigateUrl("http://localhost:3000" + "/?test=" + solution);
+
+        // Input the 1st guess
+        inputWord("fault");
+        // Input the 2nd guess
+        inputWord("river");
+        // Input the 3rd guess
+        inputWord("hobby");
+        // Input the 4th guess
+        inputWord("spoon");
+        // Input the 5th guess
+        inputWord(solution);
+
+        // I wait for 2 sec
+        iWaitForSec(2);
+
+        // Verify that the pop-up window will appear
+        elementWithXpathIsDisplayed("//*[contains(text(),'You Win!')]/..");
+
+        // Pop-up window contains the following text: "You win!"
+        String header = "You Win!";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]", header);
+
+        // Pop-up window contains the following text: "The word was: solution"
+        elementWithXpathContainsText("//*[@class = 'solution']", solution);
+
+        // Pop-up window contains the following text: "You found the solution in N guesses :)"
+        String N = "5";
+        String guesses = "in " + N + " guesses";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]/../p[2]", guesses);
+
+    }
+
+    @Test
+    // TC - Win the game on the N try
+    public void test7_winGameForSixGuesses() throws InterruptedException {
+
+        // Navigate to the page and define the solution
+        String solution = "apple";
+        navigateUrl("http://localhost:3000" + "/?test=" + solution);
+
+        // Input the 1st guess
+        inputWord("fault");
+        // Input the 2nd guess
+        inputWord("river");
+        // Input the 3rd guess
+        inputWord("hobby");
+        // Input the 4th guess
+        inputWord("spoon");
+        // Input the 5th guess
+        inputWord("fruit");
+        // Input the 6th guess
+        inputWord(solution);
+
+        // I wait for 2 sec
+        iWaitForSec(2);
+
+        // Verify that the pop-up window will appear
+        elementWithXpathIsDisplayed("//*[contains(text(),'You Win!')]/..");
+
+        // Pop-up window contains the following text: "You win!"
+        String header = "You Win!";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]", header);
+
+        // Pop-up window contains the following text: "The word was: solution"
+        elementWithXpathContainsText("//*[@class = 'solution']", solution);
+
+        // Pop-up window contains the following text: "You found the solution in N guesses :)"
+        String N = "6";
+        String guesses = "in " + N + " guesses";
+        elementWithXpathContainsText("//*[contains(text(),'You Win!')]/../p[2]", guesses);
 
     }
 
